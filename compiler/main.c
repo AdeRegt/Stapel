390274b0e547abc10fc02552ce40e1548bb212ff
Sander de Regt <alexandrosderegtvisvliet@gmail.com>
2024-02-24 22:58:59 +0100

    compiler fixes

--- compiler/main.c
+++ compiler/main.c
@@ -19,8 +19,20 @@ typedef struct{
     void *next;
 } SourceFileLineToken;
 
+typedef struct{
+    uint64_t value;
+    int size;
+    char* label_required;
+    int requires_attention;
+    char* label_defined;
+    void* next;
+} CompiledTreeValue;
+
 SourceFileLine* sourcefile_begin = NULL;
 SourceFileLine* sourcefile_now = NULL;
+
+CompiledTreeValue* targetfile_begin = NULL;
+CompiledTreeValue* targetfile_now = NULL;
 
 void grammar_error_in_token(SourceFileLineToken* token,char* message)
 {
@@ -42,6 +54,29 @@ void grammar_error_in_line(SourceFileLine* token,char* message)
     printf("       Line   :   %s\n",token->content);
     printf("       Message:   %s\n",message);
     exit(EXIT_FAILURE);
+}
+
+char *label_defined = NULL;
+
+void add_compiled_tree_value(uint64_t value, int size, char* label_required, int requires_attention)
+{
+    CompiledTreeValue* uv = (CompiledTreeValue*) malloc(sizeof(CompiledTreeValue));
+    uv->label_defined = label_defined;
+    uv->label_required = label_required;
+    uv->next = NULL;
+    uv->requires_attention = requires_attention;
+    uv->size = size;
+    uv->value = value;
+    if(targetfile_begin==NULL)
+    {
+        targetfile_begin = uv;
+    }
+    if(targetfile_now!=NULL)
+    {
+        targetfile_now->next = uv;
+    }
+    targetfile_now = uv;
+    label_defined = NULL;
 }
 
 int main(int argc,char** argv)
@@ -320,29 +355,118 @@ int main(int argc,char** argv)
         {
             SourceFileLineToken *tok = loopnow->tokens;
             if(strcmp(tok->token,"exit")==0)
-            {}
+            {
+                add_compiled_tree_value(STAPEL_INSTRUCTION_EXIT,sizeof(uint8_t),NULL,0);
+            }
             else if(strcmp(tok->token,"push")==0)
-            {}
+            {
+                if(tok->next==NULL)
+                {
+                    grammar_error_in_token(tok,"\"address\",\"value_at\" or \"value\" expected after push statement");
+                }
+                tok = tok->next;
+                if(strcmp(tok->token,"address")==0)
+                {
+                    if(tok->next==NULL)
+                    {
+                        grammar_error_in_token(tok,"variable required after \"push address\" statement");
+                    }
+                    tok = tok->next;
+                    add_compiled_tree_value(STAPEL_INSTRUCTION_PUSH_RAW_ADDR,sizeof(uint8_t),NULL,0);
+                    add_compiled_tree_value(0,sizeof(uint64_t),tok->token,1);
+                }
+                else if(strcmp(tok->token,"value_at")==0)
+                {
+                    if(tok->next==NULL)
+                    {
+                        grammar_error_in_token(tok,"integer value required after \"push value_at\" statement");
+                    }
+                    tok = tok->next;
+                    add_compiled_tree_value(STAPEL_INSTRUCTION_PUSH_ADDRESS_VALUE,sizeof(uint8_t),NULL,0);
+                    add_compiled_tree_value(0,sizeof(uint64_t),tok->token,1);
+                }
+                else if(strcmp(tok->token,"value")==0)
+                {
+                    if(tok->next==NULL)
+                    {
+                        grammar_error_in_token(tok,"integer value required after \"push value\" statement");
+                    }
+                    tok = tok->next;
+                    add_compiled_tree_value(STAPEL_INSTRUCTION_PUSH_VALUE,sizeof(uint8_t),NULL,0);
+                    add_compiled_tree_value(0,sizeof(uint64_t),NULL,1);
+                }
+                else
+                {
+                    grammar_error_in_token(tok,"\"address\",\"value_at\" or \"value\" expected");
+                }
+            }
             else if(strcmp(tok->token,"debug")==0)
-            {}
+            {
+                add_compiled_tree_value(STAPEL_INSTRUCTION_DEBUG,sizeof(uint8_t),NULL,0);
+            }
             else if(strcmp(tok->token,"add")==0)
-            {}
+            {
+                add_compiled_tree_value(STAPEL_INSTRUCTION_ADD,sizeof(uint8_t),NULL,0);
+            }
             else if(strcmp(tok->token,"sub")==0)
-            {}
+            {
+                add_compiled_tree_value(STAPEL_INSTRUCTION_SUB,sizeof(uint8_t),NULL,0);
+            }
             else if(strcmp(tok->token,"mul")==0)
-            {}
+            {
+                add_compiled_tree_value(STAPEL_INSTRUCTION_MUL,sizeof(uint8_t),NULL,0);
+            }
             else if(strcmp(tok->token,"div")==0)
-            {}
+            {
+                add_compiled_tree_value(STAPEL_INSTRUCTION_DIV,sizeof(uint8_t),NULL,0);
+            }
             else if(strcmp(tok->token,"call")==0)
-            {}
+            {
+                if(tok->next==NULL)
+                {
+                    grammar_error_in_token(tok,"expected variable name after \"call\" statement");
+                }
+                tok = tok->next;
+                add_compiled_tree_value(STAPEL_INSTRUCTION_CALL,sizeof(uint8_t),NULL,0);
+                add_compiled_tree_value(0,sizeof(uint64_t),tok->token,1);
+            }
             else if(strcmp(tok->token,"jump")==0)
-            {}
+            {
+                if(tok->next==NULL)
+                {
+                    grammar_error_in_token(tok,"expected variable name after \"jump\" statement");
+                }
+                tok = tok->next;
+                add_compiled_tree_value(STAPEL_INSTRUCTION_JUMP,sizeof(uint8_t),NULL,0);
+                add_compiled_tree_value(0,sizeof(uint64_t),tok->token,1);
+            }
             else if(strcmp(tok->token,"int")==0)
-            {}
+            {
+                add_compiled_tree_value(STAPEL_INSTRUCTION_INT,sizeof(uint8_t),NULL,0);
+            }
             else if(strcmp(tok->token,"ret")==0)
-            {}
+            {
+                add_compiled_tree_value(STAPEL_INSTRUCTION_RET,sizeof(uint8_t),NULL,0);
+            }
             else if(strcmp(tok->token,"pop")==0)
-            {}
+            {
+                if(tok->next==NULL)
+                {
+                    grammar_error_in_token(tok,"expected variable name after \"pop\" statement");
+                }
+                tok = tok->next;
+                add_compiled_tree_value(STAPEL_INSTRUCTION_POP,sizeof(uint8_t),NULL,0);
+                add_compiled_tree_value(0,sizeof(uint64_t),tok->token,1);
+            }
+            else if(strcmp(tok->token,"label")==0)
+            {
+                if(tok->next==NULL)
+                {
+                    grammar_error_in_token(tok,"name of label expected after \"label\" statement");
+                }
+                tok = tok->next;
+                label_defined = tok->token;
+            }
             else
             {
                 grammar_error_in_token(tok,"Cannot understand token");
